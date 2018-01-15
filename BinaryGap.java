9834260f2b444cc8b95b5072eb5042053e2e0a2d
Adrian Malik <adrian.malik.89@gmail.com>
2018-01-15 07:53:09 +0100

    Update BinaryGap.java

--- BinaryGap.java
+++ BinaryGap.java
@@ -1,5 +1,25 @@
-public class BinaryGap { 
-    public int solution(int array) {
+class BinaryGap {
+    
+    public int solution(int number) {
+        int maxGap = 0;
+        int currentGap = 0;
+        char[] bits = Integer.toBinaryString(number).toCharArray();
+        boolean isOpenBit = false;
+        
+        for (char bit: bits) {
+            if (bit == '1') {
+                if (isOpenBit) { 
+                    currentGap = 0;
+                } else { 
+                    isOpenBit = true;    
+                }
+            } else if (isOpenBit && bit == '0') {
+                if (maxGap < ++currentGap) {
+                    maxGap = currentGap;    
+                }
+            }
+        }
 
+        return maxGap;
     }
 }
