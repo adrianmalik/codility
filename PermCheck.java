8c32b920d435b1ebfd9a1231f5b8efd3a6e23217
Adrian Malik <adrian.malik.89@gmail.com>
2018-01-23 19:56:45 +0100

    Create PermCheck.java

---
+++ PermCheck.java
@@ -0,0 +1,17 @@
+public class PermCheck {
+  public int solution(int[] array) {
+      Arrays.sort(array);
+
+      if (array[0] != 1) {
+          return 0;
+      }
+
+      for (int i = 0; i < array.length - 1; i++) {
+          if (array[i] + 1 != array[i + 1]) {
+              return 0;
+          }
+      }
+
+      return 1;
+  }
+}
